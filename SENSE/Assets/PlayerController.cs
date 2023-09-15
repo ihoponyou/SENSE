ac10d536d5f783854ee44b37f8eb340aef5c1e9e
noahmagcalas <noah.magcalas@gmail.com>
2023-09-15 13:47:31 -0500

    rename main scene

--- SENSE/Assets/PlayerController.cs
+++ SENSE/Assets/PlayerController.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float _speed;
+
+    public float Speed
     {
-        
+        get { return _speed; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value),
+                    "Speed must be nonnegative.");
+            }
+
+            _speed = value;
+        }
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        Speed = 1.0f;
+    }
+
     void Update()
     {
-        
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input.Normalize();
+
+        transform.position += Vector3.up * Time.deltaTime * Speed;
     }
 }
