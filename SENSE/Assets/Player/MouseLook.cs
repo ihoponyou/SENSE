99b1ec439710e733c10890b5eb321b093eea66bb
noahmagcalas <noah.magcalas@gmail.com>
2023-09-19 18:08:39 -0500

    Update MouseLook.cs

--- SENSE/Assets/Player/MouseLook.cs
+++ SENSE/Assets/Player/MouseLook.cs
@@ -7,7 +7,8 @@ public class MouseLook : MonoBehaviour
     public float mouseSensitivity = 1000f;
     public Transform playerBody;
 
-    float xRotation = 0f;
+    float xRotation = 0f,
+        mouseX, mouseY;
 
     void Start()
     {
@@ -16,8 +17,8 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
