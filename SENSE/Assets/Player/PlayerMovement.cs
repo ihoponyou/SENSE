3b30bfb073a11cc57f2e5490817b55f07bdb7b2c
noahmagcalas <noah.magcalas@gmail.com>
2023-09-25 19:51:40 -0500

    smooth movement
    still floating

--- SENSE/Assets/Player/PlayerMovement.cs
+++ SENSE/Assets/Player/PlayerMovement.cs
@@ -11,58 +11,60 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private float
         maxWalkSpeed,
-        acceleration,
         gravity,
-        jumpHeight;
+        jumpHeight,
+        smoothness;
 
-    public float currentSpeed;
+    [SerializeField] private Vector3 worldVelocity;
+    [SerializeField] private bool isGrounded;
 
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float groundDistance;
 
-    [SerializeField] private Vector3 worldVelocity;
-    [SerializeField] private bool isGrounded;
+    private Vector3
+        moveDampVelocity,
+        moveVelocity = Vector3.zero,
+        inputDirection;
 
-    private Vector3 moveDirection = Vector3.zero;
-    [SerializeField] private float snappiness;
-
-    private void UpdateSpeed(Vector2 input)
+    private Vector3 input;
+    private void UpdateDirection(Vector3 input)
     {
-        if (input.magnitude > 0)
-        {
-            currentSpeed = Mathf.Min(currentSpeed + acceleration * Time.deltaTime, maxWalkSpeed);
-        }
-        else 
-            currentSpeed = Mathf.Max(currentSpeed - acceleration * Time.deltaTime, 0);
+        inputDirection = transform.TransformDirection(input);
+        moveVelocity = Vector3.SmoothDamp(
+            moveVelocity,
+            inputDirection * maxWalkSpeed,
+            ref moveDampVelocity,
+            smoothness
+        );
     }
 
-    private Vector3 inputDirection;
-    private void UpdateDirection(Vector2 input)
-    {
-        inputDirection = transform.forward * input.y + transform.right * input.x;
-        moveDirection = Vector3.Lerp(moveDirection, (input.magnitude > 0) ? inputDirection : Vector3.zero, snappiness);
-    }
-
+    private Ray groundCheckRay;
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        groundCheckRay = new Ray(groundCheck.position, Vector3.down);
+        isGrounded = Physics.Raycast(groundCheckRay, groundDistance, groundMask);
 
-        if (isGrounded && worldVelocity.y < 0)
+        if (isGrounded)
+        {
             worldVelocity.y = -2f;
 
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        UpdateSpeed(input);
+            if (Input.GetButtonDown("Jump"))
+                worldVelocity.y = Mathf.Sqrt(jumpHeight * 2f * -gravity);
+        }
+        else
+            worldVelocity.y += gravity * Time.deltaTime;
+
+        controller.Move(worldVelocity * Time.deltaTime);
+
+        input = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            0,
+            Input.GetAxisRaw("Vertical"));
+        if (input.magnitude > 1) input.Normalize();
+
         UpdateDirection(input);
 
-        Vector3 inputVelocity = moveDirection.normalized * currentSpeed * Time.deltaTime;
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            worldVelocity.y = Mathf.Sqrt(jumpHeight * 2f * -gravity);
-        }
-
-        worldVelocity.y += gravity * Time.deltaTime;
-        controller.Move(inputVelocity + worldVelocity * Time.deltaTime);
+        controller.Move(moveVelocity * Time.deltaTime);
     }
 }
